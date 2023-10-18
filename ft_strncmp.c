a0c63714e65fea7658debd80694b98108a5d8c19
BrunoKwamme <50811081+BrunoKwamme@users.noreply.github.com.>
2023-10-18 14:23:25 -0300

    header file, strncmp, gitignore update

---
+++ ft_strncmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkwamme <bkwamme@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 11:19:46 by bkwamme           #+#    #+#             */
+/*   Updated: 2023/10/18 11:19:46 by bkwamme          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+int main()
+{
+	printf("%d\n", ft_strncmp("blabla", "blabla.", 20));
+	return 0;
+}
---
+++ libft.h
@@ -0,0 +1,8 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+int	ft_atoi(char *str);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+#endif
