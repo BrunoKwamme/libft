1b3931a74e2e45e21f2a4cee089c6459ccc14cef
BrunoKwamme <50811081+BrunoKwamme@users.noreply.github.com.>
2023-10-18 16:42:11 -0300

    main added, strnstr added, libft updated

--- libft.h
+++ libft.h
@@ -1,8 +1,11 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stdio.h>
+# include <string.h>
 # include <stdlib.h>
 # include <unistd.h>
 int	ft_atoi(char *str);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int	main(int argc, char *argv[]);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 #endif
